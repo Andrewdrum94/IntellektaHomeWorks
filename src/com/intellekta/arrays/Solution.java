74b5fed48e70fce31881264370a5662d847572be
Andrey Kolesnik <grafalastormusic@mail.ru>
2023-09-04 23:41:29 +0300

    Arrays homework

---
+++ src/com/intellekta/arrays/Solution.java
@@ -0,0 +1,15 @@
+package com.intellekta.arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String[] months = {"January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"};
+
+        System.out.println(months[4]);
+        System.out.println(months[8]);
+        System.out.println(months[11]);
+        String lang = System.getProperty("user.");
+        System.out.println(lang);
+    }
+}
