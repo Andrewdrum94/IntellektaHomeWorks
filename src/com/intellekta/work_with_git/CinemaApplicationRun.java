9777fac2827960c669eb29e8fbd669a910f9a035
Andrewdrum94 <108299957+Andrewdrum94@users.noreply.github.com>
2023-11-26 19:40:51 +0300

    Update CinemaApplicationRun.java
    Забыл во второй коммит добавить файл CinemaApplicationRun.java

--- src/com/intellekta/work_with_git/CinemaApplicationRun.java
+++ src/com/intellekta/work_with_git/CinemaApplicationRun.java
@@ -10,11 +10,11 @@ public class CinemaApplicationRun {
 
     public static void main(String[] args) {
         List<Viewer> viewers = new ArrayList<>(List.of(
-                new Viewer("John", 25, 5),
-                new Viewer("Sarah", 18, 12),
-                new Viewer("Bill", 39, 173),
-                new Viewer("Purl", 89, 205),
-                new Viewer("Kitty", 13, 4)
+                new Viewer("John", 25, null),
+                new Viewer("Sarah", 18, List.of(new Cinema(), new Cinema("Harry Potter", "fantasy", 2.5))),
+                new Viewer("Bill", 39, List.of(new Cinema(), new Cinema(), new Cinema("Lord of the Rings", "fantasy", 3))),
+                new Viewer("Purl", 89, null),
+                new Viewer("Kitty", 13, List.of(new Cinema("Puaro Agata Kristi", "detective", 2), new Cinema()))
         ));
 
         System.out.println(ViewerStatistics.averageAge(viewers));
